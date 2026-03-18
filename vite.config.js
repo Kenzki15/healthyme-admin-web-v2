e71162e265bec3a9c6f1ee6de5f0a7bdbc532ec5
Kenzki <marinedeadpool1@gmail.com>
2026-03-19 04:28:52 +0800

    Remove base from Vite config to fix asset URLs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/', // Changed from absolute URL to relative path to serve assets from same domain
     plugins: [
         laravel({
             input: 'resources/js/app.js',
